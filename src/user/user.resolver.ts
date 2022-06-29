90436325b6adfa6bd6adbf5bab82711a2c76558b
Crislâine Santos <criis.santos@outlook.com>
2022-06-29 17:05:10 -0300

    feat: user resolver added

---
+++ src/user/user.resolver.ts
@@ -0,0 +1,43 @@
+import { Args, Mutation, Resolver, Query } from '@nestjs/graphql';
+import { CreateUserInput } from './dto/create-user.input';
+import { UpdateUserInput } from './dto/update-user.input';
+import { User } from './user.entity';
+import { UserService } from './user.service';
+
+@Resolver()
+export class UserResolver {
+  constructor(private userService: UserService) {}
+
+  @Query(() => [User])
+  async users(): Promise<User[]> {
+    const users = await this.userService.findAllUsers();
+    return users;
+  }
+
+  @Query(() => User)
+  async user(@Args('id') id: string): Promise<User> {
+    const user = await this.userService.findUserById(id);
+    return user;
+  }
+
+  @Mutation(() => User)
+  async createuser(@Args('data') data: CreateUserInput): Promise<User> {
+    const user = await this.userService.createUser(data);
+    return user;
+  }
+
+  @Mutation(() => User)
+  async updateUser(
+    @Args('id') id: string,
+    @Args('data') data: UpdateUserInput,
+  ): Promise<User> {
+    const updatedUser = await this.userService.updateUser(id, data);
+    return updatedUser;
+  }
+
+  @Mutation(() => Boolean)
+  async deleteUser(@Args('id') id: string): Promise<boolean> {
+    const deletedUser = await this.userService.deleteUser(id);
+    return deletedUser;
+  }
+}
