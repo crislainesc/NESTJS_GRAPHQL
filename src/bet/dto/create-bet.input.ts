da226fa4fb4b3bcade3909c051d6fa8a33746d29
Crislâine Santos <criis.santos@outlook.com>
2022-07-01 16:40:34 -0300

    feat: create bet and update bet dto added

---
+++ src/bet/dto/create-bet.input.ts
@@ -0,0 +1,20 @@
+import { InputType } from '@nestjs/graphql';
+import { IsArray, IsNotEmpty, IsString } from 'class-validator';
+
+@InputType()
+class GameObject {
+  @IsString({ message: 'The type entered for the game id field is not valid' })
+  @IsNotEmpty({ message: 'The min cart value field cannot be empty' })
+  game_id: string;
+
+  @IsArray({ message: 'The type entered for the numbers field is not valid' })
+  @IsNotEmpty({ message: 'The numbers field cannot be empty' })
+  numbers: number[];
+}
+
+@InputType()
+export class CreateBetInput {
+  @IsArray({ message: 'The type entered for the bets field is not valid' })
+  @IsNotEmpty({ message: 'The bets field cannot be empty' })
+  bets: GameObject[];
+}
---
+++ src/bet/dto/update-bet.input.ts
@@ -0,0 +1,23 @@
+import { InputType } from '@nestjs/graphql';
+import { IsArray, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+@InputType()
+class GameObject {
+  @IsString({ message: 'The type entered for the game id field is not valid' })
+  @IsNotEmpty({ message: 'The min cart value field cannot be empty' })
+  @IsOptional()
+  game_id: string;
+
+  @IsArray({ message: 'The type entered for the numbers field is not valid' })
+  @IsNotEmpty({ message: 'The numbers field cannot be empty' })
+  @IsOptional()
+  numbers: number[];
+}
+
+@InputType()
+export class UpdateBetInput {
+  @IsArray({ message: 'The type entered for the bets field is not valid' })
+  @IsNotEmpty({ message: 'The bets field cannot be empty' })
+  @IsOptional()
+  bet: GameObject;
+}
