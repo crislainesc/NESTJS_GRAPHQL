8dd7e152101b32b3649591fd47ef3d48ba9cba34
Crislâine Santos <criis.santos@outlook.com>
2022-07-05 21:14:29 -0300

    feat: typeorm, graphql, user, game, cart. bet and auth modules added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,43 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver } from '@nestjs/apollo';
+import { ConfigModule } from '@nestjs/config';
+import { join } from 'path';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { UserModule } from './user/user.module';
+import { GameModule } from './game/game.module';
+import { CartModule } from './cart/cart.module';
+import { BetModule } from './bet/bet.module';
+import { AuthModule } from './auth/auth.module';
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '.env' }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: process.env.DB_SYNC == 'true',
+    }),
+    GraphQLModule.forRoot({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      context: ({ req }) => ({ req }),
+    }),
+    UserModule,
+    GameModule,
+    CartModule,
+    BetModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
